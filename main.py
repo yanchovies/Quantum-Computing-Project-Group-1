b3082a363276dd3f29863b863b1f4999426350a7
Boaz Micah <boazmicah2@gmail.com>
2023-02-17 13:41:45 +0000

    initial commit

--- main.py
+++ main.py
@@ -1,2 +1,66 @@
-# -*- coding: utf-8 -*-
+import numpy as np
+import random
+from math import gcd
 
+def qft_matrix(n):
+    """Returns the matrix for the quantum Fourier transform on n qubits."""
+    N = 2**n
+    matrix = np.zeros((N, N), dtype=complex)
+    for i in range(N):
+        for j in range(N):
+            matrix[i][j] = np.exp(2j * np.pi * i * j / N) / np.sqrt(N)
+    return matrix
+
+def qft(statevector):
+    """Applies the quantum Fourier transform to the given statevector."""
+    n = int(np.log2(len(statevector)))
+    matrix = qft_matrix(n)
+    return np.dot(matrix, statevector)
+
+def shor(N):
+    """Implements Shor's algorithm for finding the prime factors of N."""
+    if N % 2 == 0:
+        return 2
+
+    # Choose a random integer a < N
+    a = random.randint(2, N - 1)
+    # Compute the greatest common divisor of a and N
+    g = gcd(a, N)
+    if g > 1:
+        return g
+
+    # Initialize qubits for period finding
+    n = int(np.ceil(np.log2(N**2)))
+    statevector = np.random.rand(2**n)
+
+    # Apply modular exponentiation
+    for i in range(n):
+        statevector = np.kron(np.array([(a**(2**i)) % N]), statevector)
+
+    # Apply the inverse quantum Fourier transform
+    statevector = qft(statevector)
+
+    # Measure the statevector
+    probs = np.abs(statevector)**2
+    m = np.argmax(probs)
+
+    # Use the measurement result to determine the period r
+    r = m
+    for i in range(1, n):
+        if (a**r) % N == 1:
+            break
+        r *= 2
+
+    # Compute the factors using the period
+    g = gcd(a**(r//2) - 1, N)
+    h = gcd(a**(r//2) + 1, N)
+    if g > 1 and g < N:
+        return g
+    elif h > 1 and h < N:
+        return h
+    else:
+        raise Exception("Shor's algorithm failed")
+
+# Test the implementation
+N = 15
+print("The prime factors of {} are {}".format(N, shor(N)))
