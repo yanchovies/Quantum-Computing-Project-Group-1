007898350f72ebf193c80e49a2d8cbf9db727b13
Yanchick01 <Yanegubko01@gmail.com>
2023-03-03 13:32:16 +0000

    tensor product works now!

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import objects.matrices.matrix as mat
 
 if __name__ == "__main__":
-    mat1 = mat.Matrix([[1, 3], [4, 5]])
+    mat1 = mat.Matrix([[3], [5], [7]])
     mat2 = mat.Matrix([[2, 8], [7, 4]])
 
-    result = mat1.tensor_product(mat2)
+    result = mat.tensor_product(mat1, mat2)
     print(result)
 
